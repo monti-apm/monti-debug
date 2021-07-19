a22b2a3e71eb562f9adc16a044bb17d9a046433f
zodern <zodern@users.noreply.github.com>
2021-07-19 12:24:02 -0500

    Switch to montiapm:agent

--- package.js
+++ package.js
@@ -47,7 +47,7 @@ function configure(api) {
   api.use('mongo');
   api.use('tracker');
   api.use('localstorage');
-  api.use('meteorhacks:kadira@2.23.2', 'server');
+  api.use('montiapm:agent@2.40.0', 'server');
   api.use('kadira:flow-router@2.0.0', {weak:true});
   api.use('kadira:flow-router-ssr@3.0.0', {weak:true});
   api.use('iron:router@1.0.0', {weak:true});
